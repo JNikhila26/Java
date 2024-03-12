f97505e35bdb9d335a3e2a7ac9a3465b5cb170e7
JNikhila26 <jnikhila1998@gmail.com>
2024-03-11 23:24:55 -0500

    Initial commit

---
+++ Coaching/src/FirstDay/DataTypes.java
@@ -0,0 +1,29 @@
+package FirstDay;
+
+public class DataTypes {
+    public static void main(String args[]) {
+        DataTypes mf = new DataTypes();
+        mf.myFunction();
+    }
+    static  int a = 100; // 4 Bytes
+    short s = 2; // 2 Bytes
+    long l = 1234; // 8 Bytes
+    float f = 23.4f; // 4 Bytes
+    double d = 34.999999999; // 8 Bytes
+    char c = 'N'; // Mandatory Single quotes // 1 Bytes
+    String S = "Nikhila"; //Mandatory Double quotes // 4 Bytes
+    boolean b = true; // 2 Bytes
+    void myFunction() {
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(S);
+        System.out.println(b);
+    }
+    // 1 Byte = 8 Bits
+}
+
+
---
+++ Coaching/src/FirstDay/Temp.java
@@ -0,0 +1,13 @@
+package FirstDay;
+
+public class Temp {
+    public static void  main(String args[])
+    {
+        //Temp sample = new Temp();
+        methodOne();
+    }
+    static int a = 123;
+    static void methodOne(){
+        System.out.print(a);
+    }
+}
