b1f21021867f964213a357e0890513613b2c886f
JNikhila26 <jnikhila1998@gmail.com>
2024-04-09 19:36:51 -0500

    Day14 files

---
+++ Coaching/src/Day14/Test.java
@@ -0,0 +1,9 @@
+package Day14;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = null;
+
+        System.out.println(a);
+    }
+}
---
+++ Coaching/src/Day14/threads/Thread1.java
@@ -0,0 +1,17 @@
+package Day14.threads;
+
+public class Thread1
+{
+    // Main method
+    public static void main(String argvs[])
+    {
+// creating an object of the Thread class using the constructor Thread(String name)
+        Thread t= new Thread("My first thread");
+
+// the start() method moves the thread to the active state
+        t.start();
+// getting the thread name by invoking the getName() method
+        String str = t.getName();
+        System.out.println(str);
+    }
+}
